44f4feb4919285f56a14b47f17e61c4208eef304
Arlan Reis <arlanreis615@gmail.com>
2025-01-07 22:28:37 -0300

    criei um arquivo j.s baseado em condições

---
+++ teste velocidade.js
@@ -0,0 +1,9 @@
+let vel = 40
+    if(vel > 60){ // condição simples
+        console.log(`Você ultrapassou o limite de velocidade permitido, MULTADO!`)
+    }
+    else if(vel < 50){
+        console.log(`Você está dentro do limite permitido.`)
+    }
+
+console.log(`Dirija usando cinto de segurança!`)
